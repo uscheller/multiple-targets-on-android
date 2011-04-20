9c0e5675ad85bcee5ff5171e27bdaeced74e6e5b
Ulrich Scheller <Ulrich Scheller@localhost>
2011-04-20 16:40:20 +0200

    first commit

---
+++ src/com/android/multiple_targets/Test.java
@@ -0,0 +1,15 @@
+package com.android.multiple_targets;
+
+import com.android.multiple_targets.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
