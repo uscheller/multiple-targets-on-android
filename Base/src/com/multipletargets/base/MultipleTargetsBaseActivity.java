dfdd2bf8b8c2ea2ccb61c1096ac08df9ef54cb58
Ulrich Scheller <Ulrich Scheller@localhost>
2011-10-09 19:08:35 +0200

    New version of multiple targets by using an Android Library Project, instead of the Ant script.

--- Base/src/com/multipletargets/base/MultipleTargetsBaseActivity.java
+++ Base/src/com/multipletargets/base/MultipleTargetsBaseActivity.java
@@ -1,11 +1,9 @@
-package com.android.multiple_targets;
-
-import com.android.multiple_targets.R;
+package com.multipletargets.base;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Test extends Activity {
+public class MultipleTargetsBaseActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
